8f2ad7410f3100109fee5560ada845c44d01e7a8
Dasha Chertkova <tchertkovadasha@yandex.ru>
2026-05-02 17:26:42 +0300

    PEP8 исправления

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import include
 from rest_framework.routers import DefaultRouter
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
